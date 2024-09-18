3d2c93d37e2593d6cb1731e0cd7b183a61f2b65c
Olivier Hoareau <oha@greenberets.io>
2024-09-18 09:16:09 +0200

    feat: add endpoint output

--- modules/graphql-api-regional/outputs.tf
+++ modules/graphql-api-regional/outputs.tf
@@ -8,5 +8,5 @@ output "id" {
   value = aws_appsync_graphql_api.regional.id
 }
 output "endpoint" {
-  value = "https://${aws_appsync_domain_name.regional.domain_name}/graphql"
+  value = local.custom_domain ? "https://${aws_appsync_domain_name.regional[0].domain_name}/graphql" : aws_appsync_graphql_api.regional.uris["GRAPHQL"]
 }
